33ba088e27a1b9f07ab133670b7ee259b8e2373a
lucas <lukscabral95@gmail.com>
2024-10-29 10:16:07 -0300

    ppt ui

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let human_score = 0;
 let pc_score = 0;
+let result = "";
+let rounds = 0;
+let b;
+input = document.querySelector("textarea");
 
 function get_choice_pc() {
     let pc_choice = Math.floor(Math.random()*3)
@@ -19,72 +23,80 @@ function get_choice_pc() {
         console.log("error")
     }
 }
+    function play_round(h_choice,pc_choice){  
+         
 
-function get_choice_human(){
-    let h_choice = prompt("Escolha pedra, papel ou tesoura");
-    h_choice = h_choice.toLowerCase()
-    
-    if(h_choice === "pedra"){
-        return h_choice
-        } else if(h_choice === "papel") {
-         return h_choice
-        } else if(h_choice === "tesoura") {
-            return h_choice
-        } else {
-            console.log("Digitou errado");
-        }
-}
-function play_game(){
-    let h_selection = get_choice_human();
-    let pc_selection = get_choice_pc();
-    
-    function play_round(h_choice,pc_choice){        
-        if(h_choice === pc_choice){
-            console.log("EMPATE!Jogue novamente");
+             if(h_choice === pc_choice && pc_choice != undefined){               
+                result = "empate";
+           } else if(h_choice === "pedra"){
+                if(pc_choice === "papel"){
+                    
+                    result = "Perdeu!Pedra perde pra papel!";
+                    pc_score += 1;
+                }else {
+                    
+                    result = "Venceu!Pedra ganha de tesoura!";
+                    human_score += 1;
+                }
+           } else if(h_choice === "papel"){
+                if(pc_choice === "tesoura"){
+                    
+                    result = "Perdeu!Papel perde pra tesoura!";
+                    pc_score += 1;
+                }else {
+                    
+                    result = "Venceu!Papel ganha de Pedra!";
+                    human_score += 1;
+                }
+           } else if(h_choice === "tesoura") {
+                if(pc_choice === "pedra"){
+                    
+                    result = "Perdeu!Tesoura perde pra Pedra!";
+                    pc_score += 1;
+                }else {
+                    
+                    result = "Venceu!Tesoura ganha de Papel!";
+                    human_score += 1;
+                }
+           }rounds +=1; 
+    }
+
+let buttons = document.querySelectorAll(".container > button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
             
-        } else if(h_choice === "pedra"){
-            console.log("Voce jogou pedra")
-            if(pc_choice === "papel"){
-                console.log("Voce perdeu!Pedra perde pra papel");
-                return human_score += 1;
-            }else{
-                console.log("Voce venceu!Pedra vence tesoura")
-                return pc_score +=1;
-            }
-            
-        }else if(h_choice === "papel"){
-            console.log("Voce jogou papel")
-            if(pc_choice === "pedra"){
-                console.log("Voce ganhou!Papel vence pedra")
-                return human_score += 1;
-            }else{
-                console.log("Voce perdeu!Papel perde pra tesoura")
-                return pc_score +=1;
-            }
-    
-        }else if(h_choice === "tesoura"){
-            console.log("Voce jogou tesoura")
-            if(pc_choice === "pedra"){
-                console.log("Voce perdeu!Tesoura perde pra pedra")
-                return pc_score +=1;
+            if(rounds <= 5){
+                h_choice = button.id;
+                 b = get_choice_pc();
+                 console.log("player: "+h_choice+ " pc: "+b+" round: "+rounds)
+                 input.value = "player: "+h_choice+ " pc: "+b+" round: "+rounds+"\n\n";
+                 play_round(h_choice,b)
+                console.log("scores=> player: "+human_score+" pc: "+pc_score)
+                input.value += result + "\n\nscores=> player: "+human_score+" pc: "+pc_score;
+                
+                
             } else {
-                console.log("Voce ganhou!Tesoura vence papel")
-                return human_score += 1;
+                console.log("A rodada Acabou!")
+                input.value += "\n\nA rodada Acabou!"
+                if(human_score == pc_score){
+                    console.log("EMPATE!")
+                    input.value += "\nA rodada terminou em EMPATE"
+                } else if(human_score < pc_score){
+                    console.log("Computador Ganhou!")
+                    input.value += "\nA rodada terminou e o Computador ganhou!";
+                } else {
+                    console.log("Voce ganhou!")
+                    input.value += "\nVoce ganhou!";
+                }
             }
-        }   
-    }
-    function new_round() {
-        let a = get_choice_human();
-        let b = get_choice_pc();
-        play_round(a,b);  
-    }  
-
-    play_round(h_selection,pc_selection);
-    console.log("Voce: "+human_score+" Pc: "+pc_score+" round: 1")
-    for(let i = 2; i <=5; i++){
-        new_round();
-        console.log("Voce: "+human_score+" Pc: "+pc_score+" round: "+i)  
-    }
-}
-
-play_game();
+            
+        });
+    });
+const start = document.querySelector("#start");
+start.addEventListener("click", () =>{
+    rounds = 0;
+    human_score = 0;
+    pc_score = 0;
+    input.value = "O jogo vai começar!"
+    play_round();
+})
